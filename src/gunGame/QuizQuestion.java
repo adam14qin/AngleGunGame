cac524836df5711d5cd621aa1c10ea89af9d0cff
sheastyermines <sheastyer@mines.edu>
2016-12-01 20:23:38 -0700

    Made pictures look better

--- src/gunGame/QuizQuestion.java
+++ src/gunGame/QuizQuestion.java
@@ -1,7 +1,12 @@
 package gunGame;
 
-import javax.swing.Icon;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -11,14 +16,46 @@ public class QuizQuestion extends JPanel{
 	public JRadioButton numberAnswers = new JRadioButton(); 
 	public JLabel question = new JLabel(); 
 	public JLabel picture = new JLabel(); 
+	public ButtonGroup buttonGroup = new ButtonGroup(); 
 	private int angle; 
 	private int answer; 
 	
 	public QuizQuestion(int angle, int answer) {
+		this.angle = angle; 
+		this.answer = answer;
+		this.setLayout(new BorderLayout());
 		question.setText("Enter the corresponding number for " + this.angle);
 		picture.setIcon(new ImageIcon(TargetDirection.EAST.getImage()));
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
 		
+		for(int i = 0; i<8; i++)
+		{
+			JRadioButton button = new JRadioButton("" + (i+1)); 
+			if(i==0)
+			button.setSelected(true);
+			buttonGroup.add(button);
+			buttonPanel.add(button);
+		}
+		
+		JPanel rightPanel = new JPanel(new BorderLayout());
+		JPanel wrapperPanel = new JPanel(new BorderLayout());
+		JPanel wrapperPanel2 = new JPanel(new BorderLayout());
+		
+		wrapperPanel.add(question, BorderLayout.NORTH); 
+		wrapperPanel2.add(buttonPanel, BorderLayout.CENTER); 
+		wrapperPanel.add(wrapperPanel2, BorderLayout.CENTER);
+		rightPanel.add(wrapperPanel);
+		this.add(rightPanel, BorderLayout.EAST);
+		this.add(picture, BorderLayout.WEST);
+		}
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		QuizQuestion gui = new QuizQuestion(45, 3);
+		frame.add(gui); 
+		gui.setVisible(true);
+		frame.pack();
+		frame.setVisible(true);
 	}
-	
-	
 }
--- src/gunGame/TargetDirection.java
+++ src/gunGame/TargetDirection.java
@@ -6,13 +6,13 @@ import java.awt.Toolkit;
 public enum TargetDirection {
 	
 	
-	NORTH(90, "/images/CANNON_NORTH"), SOUTH(270,"/images/CANNON_SOUTH"), EAST(0,"/images/CANNON_EAST"), WEST(180,"/images/CANNON_WEST"), SOUTH_EAST(315,"/images/CANNON_SOUTH_EAST"), SOUTH_WEST(225,"/images/CANNON_SOUTH_WEST"), NORTH_EAST(45,"/images/CANNON_NORTH_EAST"), NORTH_WEST(135,"/images/CANNON_NORTH_WEST");
+	NORTH(90, "/images/CANNON_NORTH.png"), SOUTH(270,"/images/CANNON_SOUTH.png"), EAST(0,"/images/CANNON_EAST.png"), WEST(180,"/images/CANNON_WEST.png"), SOUTH_EAST(315,"/images/CANNON_SOUTH_EAST.png"), SOUTH_WEST(225,"/images/CANNON_SOUTH_WEST.png"), NORTH_EAST(45,"/images/CANNON_NORTH_EAST.png"), NORTH_WEST(135,"/images/CANNON_NORTH_WEST.png");
 	
 	private int angle;
 	private Image image; 
     private TargetDirection(int value, String imagePath){
     this.angle = value;
-    this.setImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/"+imagePath)));
+    this.setImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource(imagePath)));
     }
     
     public int getAngle() {
